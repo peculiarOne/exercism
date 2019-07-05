3f3d37a75d6854c9833da2835b4d31539f2ef02c
peculiarOne <wayne@campbellscorner.org.uk>
2019-07-05 08:57:15 +0100

    use function name rather than closure for all(..)

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -11,7 +11,7 @@ pub fn reply(message: &str) -> &str {
     let is_shouted = {
         let alpha_only: String = trimmed.chars().filter(|c| c.is_alphabetic()).collect();
         // .all() returns true for an empty iter, so need an explicit check
-        !alpha_only.is_empty() && alpha_only.chars().all(|c| c.is_uppercase())
+        !alpha_only.is_empty() && alpha_only.chars().all(char::is_uppercase)
     };
 
     match trimmed.chars().last() {
