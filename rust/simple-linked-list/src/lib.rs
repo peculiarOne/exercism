3b642762e3f16bf0a21bd07e676c38f7c28f789a
peculiarOne <wayne@campbellscorner.org.uk>
2019-10-11 18:13:24 +0100

    simple-linked-list: cannot move out of borrowed reference

---
+++ rust/simple-linked-list/src/lib.rs
@@ -0,0 +1,94 @@
+pub struct SimpleLinkedList<T> {
+    head: Option<Elem<T>>,
+}
+
+pub struct Elem<T> {
+    value: T,
+    next_elem: Option<Box<Elem<T>>>,
+}
+
+impl<T> Elem<T> {
+    pub fn new(_value: T, next: Option<Box<Elem<T>>>) -> Self {
+        Elem {
+            value: _value,
+            next_elem: next,
+        }
+    }
+}
+
+impl<T> SimpleLinkedList<T> {
+    pub fn new() -> Self {
+        SimpleLinkedList { head: None }
+    }
+
+    pub fn len(&self) -> usize {
+        fn len_elem<T>(elem: &Option<Box<Elem<T>>>) -> usize {
+            match elem {
+                Some(e) => 1 + len_elem(&e.next_elem),
+                None => 0,
+            }
+        }
+
+        match &self.head {
+            Some(x) => 1 + len_elem(&x.next_elem),
+            None => 0,
+        }
+    }
+
+    pub fn push(&mut self, _element: T) {
+        let new_head = Elem::new(_element, self.head.take().map(Box::new));
+        self.head = Some(new_head);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        let head = self.head.take();
+        match head {
+            Some(mut h) => {
+                // map(|x| *x) dereferences our Box turning a Box<Elem<T>> into an Elem<T>
+                let v = h.next_elem.take().map(|x| *x);
+                self.head = v;
+                Some(h.value)
+                },
+            None => None,
+        }
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|e| &e.value)
+    }
+}
+
+impl<T: Clone> SimpleLinkedList<T> {
+    pub fn rev(&self) -> SimpleLinkedList<T> {
+        let mut reversed = SimpleLinkedList::new();
+        fn reverse<T: Clone>(reversed: &mut SimpleLinkedList<T>, current: Option<Elem<T>>) {
+            if let Some(element) = current {
+                reversed.push(element.value.clone());
+                reverse(reversed, element.next_elem.map(|x| *x));
+            }
+        }
+        reverse(&mut reversed, self.head);
+        unimplemented!()
+    }
+}
+
+impl<'a, T: Clone> From<&'a [T]> for SimpleLinkedList<T> {
+    fn from(_item: &[T]) -> Self {
+        let mut list = SimpleLinkedList::new();
+        for it in _item.iter() {
+            list.push(it.clone())
+        };
+        list
+    }
+}
+
+impl<T> Into<Vec<T>> for SimpleLinkedList<T> {
+    fn into(mut self) -> Vec<T> {
+        let mut vec = vec![];
+        while let Some(element) = self.pop() {
+            vec.push(element)
+        }
+        vec.reverse();
+        vec
+    }
+}
