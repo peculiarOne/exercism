bccbd9e5b15d8fd4de72748d2c9ec3a08ee78d23
waynec <waynec@optrak.com>
2019-10-11 10:30:08 +0100

    making things compile

--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -8,7 +8,7 @@ pub mod graph {
             #[derive(Debug, PartialEq)]
             pub struct Node {
                 pub name: String,
-                pub attrs: HashMap<&str, &str>,
+                pub attrs: HashMap<String, String>,
             }
 
             impl Node {
@@ -27,18 +27,29 @@ pub mod graph {
                 }
 
                 pub fn get_attr(&self, attr_name: &str) -> Option<&str> {
-                    self.attrs.get(attr_name)
+                    self.attrs.get(attr_name).map(|s| &**s)
                 }
             }
         }
 
         pub mod edge {
+            use std::collections::HashMap;
+
             #[derive(Debug, PartialEq)]
-            pub struct Edge {}
+            pub struct Edge {
+                pub attrs: HashMap<String, String>,
+            }
 
             impl Edge {
                 pub fn new(from: &str, to: &str) -> Self {
                     unimplemented!()
+                }
+
+                pub fn with_attrs(mut self, attrs: &[(&str, &str)]) -> Self {
+                    for attr in attrs {
+                        self.attrs.insert(attr.0.to_string(), attr.1.to_string());
+                    }
+                    self
                 }
             }
         }
@@ -48,13 +59,13 @@ pub mod graph {
     use graph_items::node::Node;
 
     #[derive(Debug, PartialEq)]
-    pub struct Graph {
-        pub nodes: Vec<Node>,
+    pub struct Graph<'a> {
+        pub nodes: Vec<&'a Node>,
         pub edges: Vec<Edge>,
         pub attrs: HashMap<String, String>,
     }
 
-    impl Graph {
+    impl<'a> Graph<'a> {
         pub fn new() -> Self {
             Graph {
                 nodes: vec![],
@@ -64,10 +75,13 @@ pub mod graph {
         }
 
         pub fn with_nodes(mut self, nodes: &Vec<Node>) -> Self {
-            // for node in nodes {
-            //     self.nodes.push(node);
-            // }
-            // self
+            for node in nodes {
+                self.nodes.push(node);
+            }
+            self
+        }
+
+        pub fn with_edges(mut self, edges: &Vec<Edge>) -> Self {
             unimplemented!()
         }
 
@@ -79,7 +93,7 @@ pub mod graph {
         }
 
         pub fn get_node(&self, label: &str) -> Option<&Node> {
-            self.nodes.iter().find(|&n| n.name == label)
+            self.nodes.iter().find(|n| n.name == label)
         }
 
         pub fn get_attr(&self, attr_name: &str) -> Option<&String> {
