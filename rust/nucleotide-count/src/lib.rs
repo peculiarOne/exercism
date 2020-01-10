0d7ffe2d373c24c3c801075ff2cae1a66a6d045d
peculiarOne <wayne@campbellscorner.org.uk>
2020-01-10 14:08:09 +0000

    rust: nucleotides. Tidying up with ? in place of match Err

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -6,40 +6,34 @@ pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
     if !VALID_NUCLEOTIDES.contains(&nucleotide) {
         Err(nucleotide)
     } else {
-        match get_valid_nucleotides(dna) {
-            Ok(good_dna) => Ok(get_single_nucleotide_count(&good_dna, nucleotide)),
-            Err(bad) => Err(bad),
-        }
+        Ok(get_single_nucleotide_count(
+            &get_valid_nucleotides(dna)?,
+            nucleotide,
+        ))
     }
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    match get_valid_nucleotides(dna) {
-        Ok(good_dna) => {
-            let mut counts = HashMap::new();
-            for nucleotide in &VALID_NUCLEOTIDES {
-                counts.insert(
-                    *nucleotide,
-                    get_single_nucleotide_count(&good_dna, *nucleotide),
-                );
-            }
-            Ok(counts)
+    let good_dna = get_valid_nucleotides(dna)?;
+    let mut counts = HashMap::new();
+    for nucleotide in &VALID_NUCLEOTIDES {
+        counts.insert(
+            *nucleotide,
+            get_single_nucleotide_count(&good_dna, *nucleotide),
+        );
+    }
+    Ok(counts)
+}
+
+fn get_valid_nucleotides(dna: &str) -> Result<&str, char> {
+    for c in dna.chars() {
+        if !VALID_NUCLEOTIDES.contains(&c) {
+            return Err(c);
         }
-        Err(bad) => Err(bad),
     }
+    Ok(dna)
 }
 
-fn get_valid_nucleotides(dna: &str) -> Result<Vec<char>, char> {
-    let (valid, invalid): (Vec<char>, Vec<char>) =
-        dna.chars().partition(|c| VALID_NUCLEOTIDES.contains(c));
-
-    if !invalid.is_empty() {
-        Err(invalid[0])
-    } else {
-        Ok(valid)
-    }
-}
-
-fn get_single_nucleotide_count(good_dna: &[char], nucleotide: char) -> usize {
-    good_dna.iter().filter(|&c| c == &nucleotide).count()
+fn get_single_nucleotide_count(good_dna: &str, nucleotide: char) -> usize {
+    good_dna.chars().filter(|&c| c == nucleotide).count()
 }
